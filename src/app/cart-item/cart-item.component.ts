2796444d01bc4f84c3544b6e415b36bad40f51ae
mwvane <bzishvili57@gmail.com>
2023-01-24 01:58:18 +0400

    Update styles of cart-itm-component

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { faRemove } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.css']
 })
 export class CartItemComponent {
-
+  removeIcon = faRemove
+  onDelete(){
+    
+  }
 }
