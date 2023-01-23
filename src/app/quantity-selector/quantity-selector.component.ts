f3d5af52d05400375f714efec78b8483d70b1df5
mwvane <bzishvili57@gmail.com>
2023-01-23 22:32:29 +0400

    Craete quantitySelector component

---
+++ src/app/quantity-selector/quantity-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quantity-selector',
+  templateUrl: './quantity-selector.component.html',
+  styleUrls: ['./quantity-selector.component.css']
+})
+export class QuantitySelectorComponent {
+
+}
