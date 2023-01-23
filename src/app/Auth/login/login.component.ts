7f311cb709c3b89d5cbbfbecbc100f54e8cda265
mwvane <bzishvili57@gmail.com>
2023-01-23 11:01:07 +0400

    Update styles for login component

--- src/app/Auth/login/login.component.ts
+++ src/app/Auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { faCheckDouble, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { faUserPlus, faLock, faEnvelope,faCheckDouble} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,10 +11,17 @@ export class LoginComponent {
   checkIcon = faCheckDouble
   lockIcon = faLock
   emailIcon = faEnvelope
+  userPlusIcon = faUserPlus
   loginForm: FormGroup = new FormGroup({
     username: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, Validators.required),
   });
+  constructor(private router: Router){
+
+  }
+  onSignup(){
+    this.router.navigateByUrl("signup")
+  }
   onSubmit() {}
 }
 
