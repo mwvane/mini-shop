136895fbe1a99cd62d3f508b449e448cd20721e2
mwvane <bzishvili57@gmail.com>
2023-01-24 13:55:30 +0400

    Update cart-items

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { faRemove } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-cart-item',
@@ -7,7 +7,12 @@ import { faRemove } from '@fortawesome/free-solid-svg-icons';
 })
 export class CartItemComponent {
   removeIcon = faRemove
+  @Input() cartItem:any
+  @Input() quantity:number = 1
   onDelete(){
     
+  }
+  onQuantityChange(quantity:number){
+    this.quantity = quantity
   }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
-
+export class CartComponent implements OnInit {
+  cartIcon = faCartShopping
+  cartItems: any
+  selectedQuantity: number = 1
+  constructor(private service: ItemService){
+    
+  }
+  ngOnInit(): void {
+    this.service.getCartItems().subscribe(data => {
+      console.log(data)
+      this.cartItems = data
+    })
+  }
 }
