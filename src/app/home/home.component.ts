c7b74bc71f0df75b7b8f1c152ce8f09011370d98
mwvane <bzishvili57@gmail.com>
2023-01-25 21:45:32 +0400

    Add header , add cart icon and loggedUser

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Item } from '../item';
+import { Router } from '@angular/router';
 import { ItemService } from '../item.service';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,23 +9,41 @@ import { ItemService } from '../item.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private service: ItemService) {}
+  constructor(private service: ItemService, private router: Router) {}
+  cartIcon = faCartShopping;
   items: any;
+  loggedUser: any;
+  cartItemCount: any = 0;
   ngOnInit(): void {
-    this.service.getAllItems().subscribe((data) => {
-      this.items = data;
-      console.log(this.items);
-    });
+    const user = localStorage.getItem('loggedUser');
+    if (user) {
+      this.loggedUser = JSON.parse(user);
+      this.service.getAllItems().subscribe((data) => {
+        this.items = data;
+      });
+      this.service.getCartItemsCount(this.loggedUser.id).subscribe((data) => {
+        this.cartItemCount = data;
+      });
+    } else {
+      this.router.navigateByUrl('login');
+    }
   }
   onAddToCart(item: any) {
-    let result:any
-    this.service.addToCart(item, 4, 1).subscribe((data) => {
-      if(data.hasOwnProperty('res')){
-        result = data
-        if(!result.res){
-          alert(result.errors.join("\n"))
-        }
+    let result: any;
+    this.service.addToCart(item, this.loggedUser.id, 1).subscribe((data) => {
+      result = data;
+      if (!result.res) {
+        alert(result.errors.join('\n'));
+      } else {
+        this.cartItemCount++
       }
     });
+  }
+  onLogout() {
+    localStorage.removeItem('loggedUser');
+    this.router.navigateByUrl('login');
+  }
+  onCart() {
+    this.router.navigateByUrl('cart');
   }
 }
