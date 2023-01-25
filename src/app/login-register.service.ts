b85cf2fd472fbfca699543dec9140e69462f43d4
mwvane <bzishvili57@gmail.com>
2023-01-25 16:50:44 +0400

    Add login function in loginRegisterService

--- src/app/login-register.service.ts
+++ src/app/login-register.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginRegisterService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  login(payload: any) {
+    return this.http.post('https://localhost:7129/api/LoginRegister/login', {
+      username: payload.username,
+      password: payload.password,
+    });
+  }
 }
