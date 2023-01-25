ef988526f5ad045af45c92aefa454c991d2eab54
mwvane <bzishvili57@gmail.com>
2023-01-25 21:46:23 +0400

    Update cart component functionality

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,31 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  cartIcon = faCartShopping
-  cartItems: any
-  selectedQuantity: number = 1
-  constructor(private service: ItemService){
-    
-  }
+  cartIcon = faCartShopping;
+  cartItems: any;
+  selectedQuantity: number = 1;
+  loggedUser: any;
+  constructor(private service: ItemService) {}
   ngOnInit(): void {
-    this.service.getCartItems().subscribe(data => {
-      this.cartItems = data
-    })
+    const user = localStorage.getItem('loggedUser');
+    if (user) {
+      this.loggedUser = JSON.parse(user);
+    }
+    this.service.getCartItems(this.loggedUser.id).subscribe((data) => {
+      this.cartItems = data;
+    });
   }
-  onDelete(id: number){
-    this.service.deleteCartItem(id).subscribe(data => {
-      if(data){
-        this.service.getCartItems().subscribe(data => {
-          this.cartItems = data
-        })
+  onDelete(id: number) {
+    this.service.deleteCartItem(id).subscribe((data) => {
+      if (data) {
+        this.service.getCartItems(this.loggedUser.id).subscribe((data) => {
+          this.cartItems = data;
+        });
       }
-    })
+    });
   }
 }
