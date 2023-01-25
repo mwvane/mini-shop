528f7e9a457f6bf336f738ed9006934bc57e2ffb
mwvane <bzishvili57@gmail.com>
2023-01-25 21:42:19 +0400

    Implement Login and signup functions

--- src/app/login-register.service.ts
+++ src/app/login-register.service.ts
@@ -5,11 +5,21 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class LoginRegisterService {
+  baseUrl: string = 'https://localhost:7129/api/LoginRegister';
   constructor(private http: HttpClient) {}
   login(payload: any) {
-    return this.http.post('https://localhost:7129/api/LoginRegister/login', {
+    return this.http.post(`${this.baseUrl}/login`, {
       username: payload.username,
       password: payload.password,
     });
+  }
+  signup(form: any) {
+    const user = {
+      firstname: form.firstname,
+      lastname: form.lastname,
+      email: form.email,
+      password: form.password,
+    };
+    return this.http.post(`${this.baseUrl}/signup`, user);
   }
 }
