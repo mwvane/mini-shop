e8969d56bec6b4f0c6b4057162ada529c2e73df5
mwvane <bzishvili57@gmail.com>
2023-01-25 16:54:28 +0400

    Implement form submit functionality

--- src/app/Auth/login/login.component.ts
+++ src/app/Auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faUserPlus, faLock, faEnvelope,faCheckDouble} from '@fortawesome/free-solid-svg-icons';
+import { LoginRegisterService } from 'src/app/login-register.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -16,12 +17,16 @@ export class LoginComponent {
     username: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, Validators.required),
   });
-  constructor(private router: Router){
+  constructor(private router: Router, private service: LoginRegisterService){
 
   }
   onSignup(){
     this.router.navigateByUrl("signup")
   }
-  onSubmit() {}
+  onSubmit() {
+    this.service.login(this.loginForm.value).subscribe(data => {
+      console.log(data);
+    })
+  }
 }
 
