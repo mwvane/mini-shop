987648c37488266012a1c5f861f0e0d4ae2cf11b
mwvane <bzishvili57@gmail.com>
2023-01-25 21:44:19 +0400

    Update styles for login component

--- src/app/Auth/login/login.component.ts
+++ src/app/Auth/login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { faUserPlus, faLock, faEnvelope,faCheckDouble} from '@fortawesome/free-solid-svg-icons';
+import {
+  faUserPlus,
+  faLock,
+  faEnvelope,
+  faCheckDouble,
+} from '@fortawesome/free-solid-svg-icons';
 import { LoginRegisterService } from 'src/app/login-register.service';
 @Component({
   selector: 'app-login',
@@ -9,24 +14,27 @@ import { LoginRegisterService } from 'src/app/login-register.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  checkIcon = faCheckDouble
-  lockIcon = faLock
-  emailIcon = faEnvelope
-  userPlusIcon = faUserPlus
+  checkIcon = faCheckDouble;
+  lockIcon = faLock;
+  emailIcon = faEnvelope;
+  userPlusIcon = faUserPlus;
   loginForm: FormGroup = new FormGroup({
     username: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, Validators.required),
   });
-  constructor(private router: Router, private service: LoginRegisterService){
-
-  }
-  onSignup(){
-    this.router.navigateByUrl("signup")
+  constructor(private router: Router, private service: LoginRegisterService) {}
+  onSignup() {
+    this.router.navigateByUrl('signup');
   }
   onSubmit() {
-    this.service.login(this.loginForm.value).subscribe(data => {
-      console.log(data);
-    })
+    this.service.login(this.loginForm.value).subscribe((data) => {
+      const result: any = data;
+      if (result.res) {
+        localStorage.setItem('loggedUser', JSON.stringify(result.res));
+        this.router.navigateByUrl('home');
+      } else {
+        alert(result.errors.join('\n'));
+      }
+    });
   }
 }
-
