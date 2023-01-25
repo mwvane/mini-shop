64d3d0167e40ec4665daefff9841ce462a6ab316
mwvane <bzishvili57@gmail.com>
2023-01-25 21:43:39 +0400

    create and Implement signup component

--- src/app/Auth/signup/signup.component.ts
+++ src/app/Auth/signup/signup.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  faAllergies,
+  faCheckDouble,
+  faEnvelope,
+  faLock,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { CustomValidators } from 'customValidators';
+import { LoginRegisterService } from 'src/app/login-register.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
-
+  checkIcon = faCheckDouble;
+  userIcon = faUser
+  lockIcon = faLock;
+  emailIcon = faEnvelope;
+  signupForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      firstname: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      lastname: new FormControl(null),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+    },
+    [CustomValidators.MatchValidator('password', 'confirmPassword')]
+  );
+  get passwordMatchError() {
+    return (
+      this.signupForm.getError('mismatch') &&
+      this.signupForm.get('confirmPassword')?.touched
+    );
+  }
+  constructor(private router: Router, private service: LoginRegisterService) {}
+  onSubmit(){
+    this.service.signup(this.signupForm.value).subscribe(data => {
+      const result:any = data
+      if(result.res){
+        console.log("userCreated")
+        this.router.navigateByUrl("login")
+      }
+      else{
+        alert(result.errors.join("\n"));
+      }
+    })
+  }
 }
