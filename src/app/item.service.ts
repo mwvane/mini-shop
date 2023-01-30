9d27a0e112386bfb9453a136444a0fcc3fdce876
mwvane <bzishvili57@gmail.com>
2023-01-30 23:58:39 +0400

    Fix quantity update

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -1,6 +1,7 @@
 import { EnvironmentInjector, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item } from './item';
+import { ThisReceiver } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
@@ -11,30 +12,39 @@ export class ItemService {
   getAllItems() {
     return this.http.get(`${this.baseUrl}/getAllItems`);
   }
+
   getCartItems(userId: number) {
     return this.http.get(`${this.baseUrl}/getCartItems?id=${userId}`);
   }
+
   getItemQuantity(id: number) {
     return this.http.get(`${this.baseUrl}/getItemQuantity?id=${id}`);
   }
+
   getCartItemsCount(id: number) {
-    debugger
     return this.http.get(`${this.baseUrl}/getCartCount?id=${id}`);
   }
+
   updateCartItemQuantity(id: number, quantity: number) {
     return this.http.post(`${this.baseUrl}/updateCartItemQuantity`, {
       id,
       quantity,
     });
   }
+
   deleteCartItem(id: number) {
-    return this.http.post(`${this.baseUrl}/deleteCartItem`, { id });
+    return this.http.post(`${this.baseUrl}/deleteCartItem`, id);
   }
-  addToCart(item: any, userId: number, quantity: number) {
+  addToCart(item: any, userId: number, quantity: number = 0) {
     return this.http.post(`${this.baseUrl}/addToCart`, {
       itemId: item.id,
       userId: userId,
       quantity,
     });
+  }
+
+  //admin-----------
+  removeItem(itemId:number){
+    return this.http.post(`https://localhost:7129/api/Admin/removeItem`, itemId)
   }
 }
