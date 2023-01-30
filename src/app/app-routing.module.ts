df022acd8324a4b449703f0b921a3fd348c487d1
mwvane <bzishvili57@gmail.com>
2023-01-30 23:58:05 +0400

    Remove cart rout from router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ForgetPasswordComponent } from './Auth/forget-password/forget-password.component';
 import { LoginComponent } from './Auth/login/login.component';
 import { SignupComponent } from './Auth/signup/signup.component';
-import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -12,7 +11,6 @@ const routes: Routes = [
   {path: "signup", component: SignupComponent},
   {path: "forget-password", component: ForgetPasswordComponent},
   {path: "home", component: HomeComponent},
-  {path: "cart", component: CartComponent}
 ];
 
 @NgModule({
