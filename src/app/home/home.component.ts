1b7f73f127f48ce555d932b3da9a03aaaaf5fe3b
mwvane <bzishvili57@gmail.com>
2023-01-30 23:56:36 +0400

    Add spiner loading and upadte styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,49 +9,69 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(
-    private service: ItemService,
-    private router: Router,
-  ) {}
+  constructor(private service: ItemService, private router: Router) {}
   cartIcon = faCartShopping;
-  items: any
+  items: any = [];
   loggedUser: any;
-  cartItemCount: any = 0;
-  cartItems: any = null;
+  cartItems: any = [];
   selectedQuantity: number = 1;
-  ngOnInit(): void{
-    const user = localStorage.getItem("loggedUser")
+  isItemsLoading = true;
+  isCartLoading = true;
+  ngOnInit(): void {
+    const user = localStorage.getItem('loggedUser');
     if (user) {
-      this.loggedUser = JSON.parse(user)
-      this.service.getAllItems().subscribe(data => {
+      this.loggedUser = JSON.parse(user);
+      this.service.getAllItems().subscribe((data) => {
         this.items = data;
-      })
+        this.isItemsLoading = false
+        
+      });
       this.service.getCartItems(this.loggedUser.id).subscribe((data) => {
         this.cartItems = data;
+        this.isCartLoading = false;
       });
-      ////
     } else {
       this.router.navigateByUrl('login');
     }
-    
   }
   onAddToCart(item: any) {
     let result: any;
-    this.service.addToCart(item, this.loggedUser.id, 1).subscribe((data) => {
+    this.service.addToCart(item, this.loggedUser.id).subscribe((data) => {
       result = data;
       if (!result.res) {
         alert(result.errors.join('\n'));
       } else {
-        this.cartItemCount++;
+        const newItem = {
+          id: result.res,
+          item: {id: item.id, name: item.name, price: item.price},
+          quantity: 1
+        }
+        this.cartItems.unshift(newItem)
+        item.quantity --
       }
     });
+  }
+  onCartItemChange(quantityInStock: number, item:any){
+    for(let i of this.items)
+    if(i.id === item.item.id){
+      i.quantity = quantityInStock
+      break
+    }
   }
   onCartItemDelete(id: number) {
     this.service.deleteCartItem(id).subscribe((data) => {
       if (data) {
-        this.service.getCartItems(this.loggedUser.id).subscribe((data) => {
-          this.cartItems = data;
-        });
+        for(let [index,cartItem] of this.cartItems.entries()){
+          if(cartItem.id === id){
+            for(let item of this.items){
+              if(item.id === cartItem.item.id){
+                item.quantity += cartItem.quantity
+              }
+            }
+            this.cartItems.splice(index,1)
+            break
+          }
+        }
       }
     });
   }
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Item } from '../item';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faPen, faRemove, faPlus } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
@@ -8,9 +8,25 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 })
 export class ItemComponent {
   @Input() item: any;
+  @Input() role: string = 'user'
   @Output() addToCart = new EventEmitter();
+  @Output() editItem = new EventEmitter<number>();
+  @Output() removeItem = new EventEmitter<number>();
   cartIcon = faCartShopping;
+  editIcon = faPen
+  removeIcon = faRemove
+  addIcon = faPlus
   onAddToCart() {
     this.addToCart.emit();
+  }
+  onRemove(){
+    if(this.role === 'admin'){
+      this.removeItem.emit(this.item.id)
+    }
+  }
+  onEdit(){
+    if(this.role === 'admin'){
+      this.editItem.emit(this.item.id)
+    }
   }
 }
