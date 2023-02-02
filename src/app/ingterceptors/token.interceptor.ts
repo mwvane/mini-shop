b5cce276a8ebd5a19b04af4ca71464fc9facd202
mwvane <bzishvili57@gmail.com>
2023-02-02 21:35:56 +0400

    Craete Interseptor

---
+++ src/app/ingterceptors/token.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { LoginRegisterService } from '../login-register.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(
+    private authService: LoginRegisterService,
+    private router: Router
+  ) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = this.authService.getToken();
+    if (token) {
+      request = request.clone({
+        setHeaders: { Authorization: `Bearer ${token}` },
+      });
+    }
+    return next.handle(request).pipe(
+      catchError((error: any) => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 401) {
+            alert('User Expired');
+            this.router.navigateByUrl('login');
+          }
+        }
+        return throwError(() => new Error('Something went wrong'));
+      })
+    );
+  }
+}
