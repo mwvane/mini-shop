06002327724b452d5419f21f365875d25b80271b
mwvane <bzishvili57@gmail.com>
2023-02-11 10:21:03 +0400

    Make some changes

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { faRemove } from '@fortawesome/free-solid-svg-icons';
 import { ItemService } from '../service/item.service';
 import { ToastrService } from 'ngx-toastr';
 
@@ -8,31 +7,31 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.css'],
 })
-export class CartItemComponent implements OnInit {
-  removeIcon = faRemove;
+export class CartItemComponent {
   @Input() cartItem: any;
   @Output() remove = new EventEmitter();
-  @Output() change = new EventEmitter()
+  @Output() change = new EventEmitter();
+  @Output() open = new EventEmitter();
   maxQuantity: any = 10;
 
-  constructor(private service: ItemService, private msgService: ToastrService) {}
-  ngOnInit(): void {
-    console.log(this.cartItem)
-    debugger
-  }
-  onDelete() {
+  constructor(
+    private service: ItemService,
+    private msgService: ToastrService
+  ) {}
+  onDelete(e: Event) {
+    e.stopPropagation();
     this.remove.emit();
   }
   onQuantityIncrease() {
     let result: any;
-    console.log(this.cartItem)
+    console.log(this.cartItem);
     this.service
       .updateCartItemQuantity(this.cartItem.id, 1)
       .subscribe((data) => {
         result = data;
         if (result.res != null) {
           this.cartItem.quantity++;
-          this.change.emit(result.res)
+          this.change.emit(result.res);
         } else {
           this.msgService.warning(result.errors.join('\n'));
         }
@@ -46,10 +45,13 @@ export class CartItemComponent implements OnInit {
         result = data;
         if (result.res != null) {
           this.cartItem.quantity--;
-          this.change.emit(result.res)
+          this.change.emit(result.res);
         } else {
           this.msgService.warning(result.errors.join('\n'));
         }
       });
+  }
+  onItemClick() {
+    this.open.emit(this.cartItem);
   }
 }
