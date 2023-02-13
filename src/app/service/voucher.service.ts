29cb61df3ac714e784edfc70495d0e9861614744
mwvane <bzishvili57@gmail.com>
2023-02-14 01:20:13 +0400

    Implement vaucher createfunction

--- src/app/service/voucher.service.ts
+++ src/app/service/voucher.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { subscribeOn } from 'rxjs';
+import { Result } from '../Model/result';
 import { Voucher } from '../Model/voucher';
 
 @Injectable({
@@ -8,7 +10,17 @@ import { Voucher } from '../Model/voucher';
 export class VoucherService {
 
   constructor(private http: HttpClient) { }
+  private baseUrl:string = "https://localhost:7129/api/Voucher"
   getAllVouchers(){
-    this.http.get<Voucher[]>()
+    return this.http.get<Voucher[]>(`${this.baseUrl}/getAllVouchers`)
+  }
+  generateKey(){
+    return this.http.get<Result>(`https://localhost:7129/api/Voucher/generateVoucherKey`);
+  }
+  create(voucher: Voucher){
+    return this.http.post<Result>(`${this.baseUrl}/createVoucher`, voucher)
+  }
+  update(voucher: Voucher){
+    
   }
 }
