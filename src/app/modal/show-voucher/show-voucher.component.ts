a0362306ada9c6ddc22c79c1e1507f96dac38810
mwvane <bzishvili57@gmail.com>
2023-02-23 00:30:01 +0400

    Fix incorrect displaing date format

--- src/app/modal/show-voucher/show-voucher.component.ts
+++ src/app/modal/show-voucher/show-voucher.component.ts
@@ -1,16 +1,28 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import * as moment from 'moment';
+import { Constants } from 'src/app/constants/constants';
 
 @Component({
   selector: 'app-show-voucher',
   templateUrl: './show-voucher.component.html',
-  styleUrls: ['./show-voucher.component.css']
+  styleUrls: ['./show-voucher.component.css'],
 })
 export class ShowVoucherComponent {
   @Input() data: any;
   @Input() openDialog: boolean = false;
   @Output() close = new EventEmitter();
+  @Output() add = new EventEmitter();
   onClose() {
     this.close.emit();
     this.openDialog = false;
+  }
+  onAdd() {
+    this.add.emit();
+  }
+  dateFormat(date: any, format: string = Constants.DATE_FORMAT) {
+    if (date) {
+      return moment(date).format(format);
+    }
+    return null;
   }
 }
