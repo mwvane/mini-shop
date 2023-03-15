b04401bbf2712a580da98afff9d4e281c4702d45
mwvane <bzishvili57@gmail.com>
2023-03-15 22:48:24 +0400

    Create file servoce

---
+++ src/app/service/file.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Header } from 'primeng/api';
+import { AppComponent } from '../app.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileService {
+  constructor(private http: HttpClient) {}
+
+  baseUrl: string = 'https://localhost:7129/api/File';
+
+  uploadProductImages(productId: number, images: any) {
+    debugger;
+    let formData = new FormData();
+    for (let i = 0; i < images.length; i++) {
+      formData.append("files", images[i]);
+    }
+    formData.append('productId', String(productId));
+    return this.http.post(`${this.baseUrl}/uplaodFile`, formData);
+  }
+}
